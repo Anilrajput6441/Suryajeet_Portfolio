8e003391d81815b19fa53d27f665564fc4820b61
anilrajput6441 <anilrajput6441@gmail.com>
2025-09-04 10:22:00 +0530

    Update Banner component for improved responsiveness: adjust height for mobile view, enhance image handling with separate mobile and desktop banners, and refine alt text for accessibility.

--- client/components/Banner.jsx
+++ client/components/Banner.jsx
@@ -4,12 +4,23 @@ import Image from "next/image";
 
 const Banner = () => {
   return (
-    <section id="banner" className="relative w-full h-screen overflow-hidden">
+    <section
+      id="banner"
+      className="relative w-full h-[70vh] -mt-1  overflow-hidden"
+    >
+      {/* Mobile Banner - Hidden on desktop */}
+      <Image
+        src="/Mobile-banner.png"
+        alt="Mobile banner"
+        fill
+        className="object-contain md:hidden"
+      />
+      {/* Desktop Banner - Hidden on mobile */}
       <Image
         src="/Busniess-Web-Banner-15.jpg"
-        alt="banner"
+        alt="Desktop banner"
         fill
-        className="object-cover"
+        className="object-cover hidden md:block"
       />
     </section>
   );
